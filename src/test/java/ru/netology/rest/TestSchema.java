f0e00c5a1fb8b804f54dde33df6e0a127fb1cfe6
Anna Gosteva <gostevaay@yandex.ru>
2023-04-18 18:16:30 +0300

    add schema

---
+++ src/test/java/ru/netology/rest/TestSchema.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestSchema {
+    @Test
+    public void shouldMatchSchemaTest() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .body("[0].currency", equalTo("RUB"))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+}
